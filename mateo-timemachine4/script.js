39bfc939bd9182c82fd3a3b9d9a470c1c5bdeb0b
smadelta <93409120+smadelta@users.noreply.github.com>
2022-01-21 08:28:37 +0100

    Add files via upload

--- mateo-timemachine4/script.js
+++ mateo-timemachine4/script.js
@@ -6,8 +6,6 @@ let hamburgerMenu = document.querySelector('.hamburger-menu');
 let container = document.querySelector('.container')
 let sidebar = document.querySelector('.sidebar');
 
-/*Hide sidebar on click anywhere inside the container EXCEPT the menu icons OR inside the sidebar section*/
-
 container.addEventListener("click",(e)=>{
     let target = e.target;
     
@@ -27,38 +25,27 @@ const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
-  // Get current class
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for next slide
   if (current.nextElementSibling) {
-    // Add current to next sibling
     current.nextElementSibling.classList.add('current');
   } else {
-    // Add current to start
     slides[0].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
 };
 
 const prevSlide = () => {
-  // Get current class
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for prev slide
   if (current.previousElementSibling) {
-    // Add current to prev sibling
     current.previousElementSibling.classList.add('current');
   } else {
-    // Add current to last
     slides[slides.length - 1].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
 };
 
-// Button events
 next.addEventListener('click', e => {
   nextSlide();
   if (auto) {
@@ -77,6 +64,5 @@ prev.addEventListener('click', e => {
 
 // Auto slide
 if (auto) {
-  // Run next slide at interval time
   slideInterval = setInterval(nextSlide, intervalTime);
 }
