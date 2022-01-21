29eeb7dfb055d87d557d51b5aa5a490237f5c2e2
smadelta <93409120+smadelta@users.noreply.github.com>
2022-01-21 08:18:23 +0100

    Add files via upload

--- mateo-timemachine4/script.js
+++ mateo-timemachine4/script.js
@@ -2,54 +2,81 @@ document.querySelector('.hamburger-menu').addEventListener("click",()=>{
     document.querySelector(".container").classList.toggle("change");
 });
 
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+let container = document.querySelector('.container')
+let sidebar = document.querySelector('.sidebar');
 
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
-let currentSlide = 1;
+/*Hide sidebar on click anywhere inside the container EXCEPT the menu icons OR inside the sidebar section*/
 
-var manualNav = function(manual){
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
+container.addEventListener("click",(e)=>{
+    let target = e.target;
+    
+    if (container.classList.contains("change") && target!=sidebar && target!=hamburgerMenu){
 
-    btns.forEach((btn) => {
-      btn.classList.remove('active');
-    });
-  });
+            container.classList.remove("change");
 
-  slides[manual].classList.add('active');
-  btns[manual].classList.add('active');
-}
+  }
+    
+})
 
-btns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    manualNav(i);
-    currentSlide = i;
-  });
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true; // Auto scroll
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+// Button events
+next.addEventListener('click', e => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
 });
 
-var repeat = function(activeClass){
-  let active = document.getElementsByClassName('active');
-  let i = 1;
-
-  var repeater = () => {
-    setTimeout(function(){
-      [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove('active');
-      });
-
-    slides[i].classList.add('active');
-    btns[i].classList.add('active');
-    i++;
-
-    if(slides.length == i){
-      i = 0;
-    }
-    if(i >= slides.length){
-      return;
-    }
-    repeater();
-  }, 5000);
+prev.addEventListener('click', e => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
   }
-  repeater();
+});
+
+// Auto slide
+if (auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
 }
-repeat();
